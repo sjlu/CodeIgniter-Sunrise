4c44658f142b6d2df5f6e4cdc46fd67db9c20e79
Steven Lu <tacticalazn@gmail.com>
2012-12-07 14:40:14 -0500

    Fixing routes.

--- application/config/routes.php
+++ application/config/routes.php
@@ -38,11 +38,8 @@
 |
 */
 
-$route['default_controller'] = "view/index/fashion";
+$route['default_controller'] = "welcome";
 $route['404_override'] = '';
-
-$route['(:any)'] = "view/index/$1";
-
 
 /* End of file routes.php */
 /* Location: ./application/config/routes.php */
