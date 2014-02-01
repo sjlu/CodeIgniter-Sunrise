90d0c18095fa35c98d28d2cfd65e2df38cbb5952
Steven Lu <tacticalazn@gmail.com>
2014-02-01 11:44:13 -0500

    Moving to bower_components

--- application/views/header.php
+++ application/views/header.php
@@ -11,7 +11,10 @@
 
   <link href="<?php echo base_url('assets/styles.css'); ?>" rel="stylesheet">
 
-  <script src="<?php echo base_url('assets/vendor/jquery/jquery.js'); ?>"></script>
+  <script src="<?php echo base_url('bower_components/jquery/jquery.js'); ?>"></script>
+  <script src="<?php echo base_url('bower_components/jquery-ui/ui/jquery-ui.js'); ?>"></script>
+  <script src="<?php echo base_url('bower_components/lodash/dist/lodash.js'); ?>"></script>
+  <script src="<?php echo base_url('bower_components/twitter/dist/js/bootstrap.js'); ?>"></script>
   <script src="<?php echo base_url('assets/scripts.js'); ?>"></script>
 </head>
 <body>
